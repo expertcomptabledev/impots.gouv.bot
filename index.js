2fe01ba03196576b75f478585a50e7e466c7d66b
Pierre BOURDU <pierre@qrea.io>
2019-03-06 17:20:10 +0100

    fix index

--- index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-// EXPORT ACTIONS DEFAULT MODULE
-var actions = require("./commands/actions");
-exports["default"] = actions;
--- index.ts
+++
@@ -1,3 +0,0 @@
-// EXPORT ACTIONS DEFAULT MODULE
-import * as actions from './commands/actions';
-export default actions;

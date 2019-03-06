0160f7b61e3c38ba69a74971d773cf839d5ff09d
Pierre BOURDU <pierre@qrea.io>
2019-03-06 17:31:11 +0100

    updtea export inside commandesé gi

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,6 @@
-export * from './login';
+export * from './cfe';
 export * from './companies';
 export * from './declarations';
+export * from './get-fiscal-account';
+export * from './get-fiscal-links';
+export * from './login';

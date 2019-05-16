0b3740c3ccd95fb35a5f49ddab46d1e1476f7e26
Pierre BOURDU <pierre.bourdu@gmail.com>
2019-05-16 19:34:42 +0200

    bla

--- src/actions/declare-informations.ts
+++ src/actions/declare-informations.ts
@@ -97,15 +97,17 @@ export const getDeclareInformations = async (
 
         const pageDeclarations = await newPageHandler();
 
-        await pageDeclarations.waitForSelector('#espaceDialogue', { timeout: TIMEOUT });
+        await pageDeclarations.waitForSelector('#periodeCalcule > table', { timeout: TIMEOUT });
         // await pageDeclarations.elaluate(() => page.querySelector('#periodeCalcule > table > tbody > tr:last-child > td > a', el => el.click()));
         // // await pageDeclarations.goto('https://cfspro.impots.gouv.fr/efitvamapi/afficherContexte2.html', { timeout: TIMEOUT });
         // await pageDeclarations.waitForNavigation({ timeout: TIMEOUT });
         // await pageDeclarations.waitForSelector('#espaceDialogue', { timeout: TIMEOUT });
         
         const declarations: Array<DeclareInformation> = await pageDeclarations.evaluate(() => {
+
             const tableau = document.querySelector('#periodeCalcule > table');
             const lines = tableau.querySelectorAll('tr');
+
             const declarations = [];
             for (let index = 1; index < lines.length; index++) {
                 const line = lines[index];

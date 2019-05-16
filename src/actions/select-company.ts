7af9ef5640f2391c0a23011ad8af88b7bd79e88e
Pierre BOURDU <pierre.bourdu@gmail.com>
2019-05-16 16:58:46 +0200

    minor fix

--- src/actions/select-company.ts
+++ src/actions/select-company.ts
@@ -17,18 +17,16 @@ export const selectCompany = async (email: string, password: string, siren: stri
 
   await page.waitForSelector(formSelector, { timeout: TIMEOUT });
 
-  // fill form
-  await Promise.all([
-      page.type('#siren0', siren.substring(0,1)),
-      page.type('#siren1', siren.substring(1,2)),
-      page.type('#siren2', siren.substring(2,3)),
-      page.type('#siren3', siren.substring(3,4)),
-      page.type('#siren4', siren.substring(4,5)),
-      page.type('#siren5', siren.substring(5,6)),
-      page.type('#siren6', siren.substring(6,7)),
-      page.type('#siren7', siren.substring(7,8)),
-      page.type('#siren8', siren.substring(8,9)),
-  ])
+    // fill form
+    await  page.type('#siren0', siren.substring(0,1));
+    await  page.type('#siren1', siren.substring(1,2));
+    await  page.type('#siren2', siren.substring(2,3));
+    await  page.type('#siren3', siren.substring(3,4));
+    await  page.type('#siren4', siren.substring(4,5));
+    await  page.type('#siren5', siren.substring(5,6));
+    await  page.type('#siren6', siren.substring(6,7));
+    await  page.type('#siren7', siren.substring(7,8));
+    await  page.type('#siren8', siren.substring(8));
 
   // send
   const [response] = await Promise.all([
